7b99b147d40f072d9fe194fe0d00b7f75ae366d9
Marcus Olsson <ninjasinventedphysics@gmail.com>
2017-04-11 09:31:05 +0200

    Update with latest changes

--- 2-basics/loops/example1.go
+++ 2-basics/loops/example1.go
@@ -3,15 +3,21 @@
 package main
 
 func example() {
+	// START FIRST OMIT
 	for i := 0; i < 10; i++ {
 
 	}
+	// END FIRST OMIT
 
+	// START SECOND OMIT
 	for i < 10 {
 
 	}
+	// END SECOND OMIT
 
+	// START THIRD OMIT
 	for {
 
 	}
+	// END THIRD OMIT
 }
