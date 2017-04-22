12672581ecb1521fc6a379163f053f1a6cdb1cb0
Marcus Olsson <ninjasinventedphysics@gmail.com>
2017-04-22 13:21:47 +0200

    Update with latest changes

--- 2-basics/error/example1.go
+++ 2-basics/error/example1.go
@@ -1,11 +1,15 @@
-// +build OMIT
-
 package error
+
+import (
+	"errors"
+	"fmt"
+)
 
 type error interface {
 	Error() string
 }
 
+// START MYERROR OMIT
 type myError struct {
 	reason string
 }
@@ -14,9 +18,20 @@ func (e myError) Error() string {
 	return e.reason
 }
 
-func add(x,y) (int, error) {
+// END MYERROR OMIT
+
+func add(x, y int) (int, error) {
 	if x > 10 && y > 10 {
 		return 0, errors.New("number too big")
 	}
-	return x + y
+	return x + y, nil
+}
+
+func example() {
+	// START HANDLE OMIT
+	sum, err := add(4, 5)
+	if err != nil {
+		fmt.Println("something bad happened")
+	}
+	// END HANDLE OMIT
 }
